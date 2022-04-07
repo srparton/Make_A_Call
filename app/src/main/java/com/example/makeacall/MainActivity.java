f3d74f90b570013f1fc2f351d248315b0c3a67dd
nick-g99 <103017771+nick-g99@users.noreply.github.com>
2022-04-06 22:23:22 -0500

    Should function now.
    It currently crashes when pressing "find" but I think it's because the database cannot be found yet...

--- app/src/main/java/com/example/makeacall/MainActivity.java
+++ app/src/main/java/com/example/makeacall/MainActivity.java
@@ -10,53 +10,78 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String AUTHORITY = "com.example.phonebook.MyContentProvider";
     private static final String TABLE_CONTACTS = "contacts";
-    public static final String COLUMN_ID = "id";
     public static final String COLUMN_FNAME = "fname";
     public static final String COLUMN_LNAME = "lname";
     public static final String COLUMN_PHONENUM = "phonenum";
+    EditText fnameBox, lnameBox;
+    TextView phnNumBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/"+ TABLE_CONTACTS);
-        String[] projection = {COLUMN_ID, COLUMN_FNAME,
-                COLUMN_LNAME, COLUMN_PHONENUM};
-        //To get a list of the items from the MyContentProvider, we use ContentResolver.query()
-        ContentResolver crInstance = getContentResolver();
-        // the arguments are (1) uri to the table,
-        // (2) selection criteria, (3) selection criteria, (4) sort of the order of return
-        Cursor cursor = crInstance.query(CONTENT_URI,projection,null,null,null);
-        TextView phoneNum = findViewById(R.id.phone_number);
-        String s = "The list of item(s) are:\n\n";
-        if(!cursor.moveToFirst())
-            s = "no result to display";
-        else
-            do{
-                s += String.format("%-10s\t%-10s\t%-10s\n",cursor.getString(0),
-                        cursor.getString(1), cursor.getString(2));
-            }while (cursor.moveToNext());
-        phoneNum.setText(s);
-    }
-/*
-    public void makeCall(View view) {
-        if(){
-            String phone = et1.getText().toString() + et2.getText().toString() + et3.getText().toString();
 
-            try {
-                Uri uri = Uri.parse("tel:" + phone);
-                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
-                Log.i("Make A Call app", phone);
-                startActivity(intent);
-            } catch (ActivityNotFoundException e) {
-                Toast.makeText(getApplicationContext(), "Application failed", Toast.LENGTH_LONG).show();
+        fnameBox = findViewById(R.id.fname);
+        lnameBox = findViewById(R.id.lname);
+        phnNumBox = findViewById(R.id.phone_number);
+    }
+
+    public void find(View view) {
+        Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + TABLE_CONTACTS);
+        String[] projection = {COLUMN_FNAME, COLUMN_LNAME, COLUMN_PHONENUM};
+        // To get the list of items from MyContentProvider, use ContentResolver.query()
+        ContentResolver crInstance = getContentResolver();
+        Cursor cursor = crInstance.query(CONTENT_URI, projection,
+                null, null, null);
+
+        // This logic is kind of awful...
+        boolean b = true;
+        if (!cursor.moveToFirst()) {
+            phnNumBox.setText("There are no contacts in the phone book.");
+            b = false;
+        }
+        while (b) {
+            if ((cursor.getString(0).equals(fnameBox.getText().toString())) &&
+                    (cursor.getString(1).equals(lnameBox.getText().toString()))) {
+                phnNumBox.setText(cursor.getString(2));
+                break;
+            }
+            else if (!cursor.moveToNext()) {
+                phnNumBox.setText("Contact not found.");
+                break;
             }
         }
-    }*/
+        cursor.close();
+    }
+
+    public void makeCall(View view) {
+        String PN = "";
+        if (phnNumBox.getText().toString().length() == 12) {
+            PN = phnNumBox.getText().toString().substring(0, 3) +
+                    phnNumBox.getText().toString().substring(4, 7) +
+                    phnNumBox.getText().toString().substring(8, 12);
+        }
+        else if (phnNumBox.getText().toString().length() == 8) {
+            PN = phnNumBox.getText().toString().substring(0, 3) +
+                    phnNumBox.getText().toString().substring(4, 8);
+        }
+        if (!PN.equals("")) {
+            try {
+                Uri uri = Uri.parse("tel:" + PN);
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(this, "Application failed", Toast.LENGTH_LONG).show();
+            }
+        }
+        else
+            Toast.makeText(this, "Find an existing contact", Toast.LENGTH_LONG).show();
+    }
 }
